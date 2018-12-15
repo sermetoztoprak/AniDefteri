fd8c8aa612ed648b5c32145d58ada03cbaea0437
sermetoztoprak <sermetoztoprak@gmail.com>
2018-12-15 16:20:21 +0300

    -Login design -Add post menu create

--- app/src/main/java/com/finansalbt/anidefteri/FeedActivity.java
+++ app/src/main/java/com/finansalbt/anidefteri/FeedActivity.java
@@ -1,9 +1,33 @@
 package com.finansalbt.anidefteri;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_post,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.add_post){
+            Intent intent = new Intent(getApplicationContext(),UploadActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
