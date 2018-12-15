6187af63bcfc0e574f23211a9272eda2af7d73f0
sermetoztoprak <sermetoztoprak@gmail.com>
2018-12-15 17:16:52 +0300

    Upload Activity design-Selected image storage get permission

--- app/src/main/java/com/finansalbt/anidefteri/UploadActivity.java
+++ app/src/main/java/com/finansalbt/anidefteri/UploadActivity.java
@@ -1,13 +1,82 @@
 package com.finansalbt.anidefteri;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class UploadActivity extends AppCompatActivity {
+
+    EditText postDescriptionText;
+    ImageView postImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        postImageView=findViewById(R.id.postImageView);
+        postDescriptionText=findViewById(R.id.postDescriptionText);
+    }
+
+    public void upload (View view){
+
+    }
+    public void selectImage (View view) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }
+        else {
+            Intent intent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,2);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode==1){
+            if (grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Intent intent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,2);
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode==2 && resultCode == RESULT_OK && data != null){
+            Uri image = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),image);
+                postImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
